d16f242a55dd7aa49e43f0a454708f4a58f11893
yusuf <yusufgad24@gmail.com>
2023-11-29 16:37:07 +0300

    shift unshift and get methods

--- DATA-STRUCTURES/01-singly-linked-lists.js
+++ DATA-STRUCTURES/01-singly-linked-lists.js
@@ -60,6 +60,51 @@ class SinglyLinkedList {
     return current;
   }
 
+  shift() {
+    if (!this.head) return undefined;
+
+    const curHead = this.head;
+    this.head = curHead.next;
+
+    this.length--;
+
+    if (this.length === 0) this.tail = null;
+
+    return curHead.value;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+
+  get(index) {
+    // edge case
+    if (index < 0 || index >= this.length) return null;
+
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== index) {
+      console.log(counter, this.head.value);
+      current = current.next;
+      counter++;
+    }
+
+    return current;
+  }
+
   traverse() {
     let current = this.head;
     while (current) {
@@ -74,14 +119,17 @@ const list2 = new SinglyLinkedList();
 
 console.log(list1.push("11"));
 console.log(list1.push("12"));
-console.log(list1.push("13"));
-console.log(list1.push("14"));
 
 list1.traverse();
 
-console.log(list1.pop());
-console.log(list1.pop());
-console.log(list1.pop());
-console.log(list1.pop());
-console.log(list1.pop());
+// console.log(list1.shift());
+// console.log(list1);
+
+list1.unshift("hi");
+
+console.log(list2.unshift("hello"));
+console.log(list2.unshift("hello"));
+
 console.log(list1);
+
+console.log(list1.get(1));
