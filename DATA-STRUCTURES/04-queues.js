2f46c3edbcd4a3dc4f427f831a7d8c580fc64f33
yusuf <yusufgad24@gmail.com>
2023-12-09 17:43:57 +0300

    Queue Data Structure

---
+++ DATA-STRUCTURES/04-queues.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = this.first;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    const deletedNode = this.first;
+
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+
+    this.size--;
+
+    return deletedNode.value;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(4);
+queue.enqueue(6);
+queue.enqueue(8);
+queue.enqueue(2);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+
+console.log(queue);
